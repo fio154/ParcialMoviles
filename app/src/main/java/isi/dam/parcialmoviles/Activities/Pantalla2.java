c56b19d75025f17ae60417c940028e8640003838
fio154 <51134863+fio154@users.noreply.github.com>
2020-11-20 22:32:07 -0300

    parcial terminado

--- app/src/main/java/isi/dam/parcialmoviles/Activities/Pantalla2.java
+++ app/src/main/java/isi/dam/parcialmoviles/Activities/Pantalla2.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -28,6 +29,9 @@ public class Pantalla2 extends AppCompatActivity {
     ListView listView;
     ArrayAdapter<String> myAdapter;
     ArrayList<String> lista = new ArrayList<String>();
+    Context context = this;
+    String nombre = null;
+    String apellido = null;
 
 
     @Override
@@ -42,43 +46,52 @@ public class Pantalla2 extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.listView);
 
-
         String guardar =getIntent().getStringExtra("guardar");
         if(guardar.equals("nombre")){
-            String nombre =getIntent().getStringExtra("nombre");
-            lista.add(nombre);
+            nombre =getIntent().getStringExtra("nombre");
+            RegistrarNombre(nombre);
+            mostrarNombre();
+            //lista.add(nombre);
         }else if(guardar.equals("apellido")){
-            String apellido =getIntent().getStringExtra("apellido");
-            lista.add(apellido);
+            apellido =getIntent().getStringExtra("apellido");
+            RegistrarApellido(apellido);
+            mostrarApellido();
+            //lista.add(apellido);
         }
+
 
         myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lista);
         listView.setAdapter(myAdapter);
     }
 
-    public void Registrar(View view){
+    public void RegistrarNombre(String nombre){
         AdminSQLite admin = new AdminSQLite(this, "administracion", null, 1);
         SQLiteDatabase baseDeDatos = admin.getWritableDatabase();
 
-        String guardar =getIntent().getStringExtra("guardar");
-        String nombre = null;
-        String apellido = null;
-        if(guardar.equals("nombre")){
-            nombre =getIntent().getStringExtra("nombre");
-        }else if(guardar.equals("apellido")){
-            apellido =getIntent().getStringExtra("apellido");
-        }
+        nombre =getIntent().getStringExtra("nombre");
 
         ContentValues registro = new ContentValues();
         registro.put("nombre", nombre);
-        registro.put("apellido", apellido);
 
         baseDeDatos.insert("persona", null, registro);
         baseDeDatos.close();
 
     }
 
-    public void mostrarDatos(View view){
+    public void RegistrarApellido(String apellido){
+        AdminSQLite admin = new AdminSQLite(this, "administracion", null, 1);
+        SQLiteDatabase baseDeDatos = admin.getWritableDatabase();
+
+        apellido =getIntent().getStringExtra("apellido");
+
+        ContentValues registro = new ContentValues();
+        registro.put("apellido", apellido);
+
+        baseDeDatos.insert("persona", null, registro);
+        baseDeDatos.close();
+    }
+
+    public void mostrarNombre(){
         AdminSQLite admin = new AdminSQLite(this, "administracion", null, 1);
         SQLiteDatabase baseDeDatos = admin.getWritableDatabase();
 
@@ -86,12 +99,24 @@ public class Pantalla2 extends AppCompatActivity {
 
         if(fila.moveToFirst()){
             lista.add(fila.getString(0));
+            baseDeDatos.close();
+        }else{
+            baseDeDatos.close();
+        }
+    }
+
+    public void mostrarApellido(){
+        AdminSQLite admin = new AdminSQLite(this, "administracion", null, 1);
+        SQLiteDatabase baseDeDatos = admin.getWritableDatabase();
+
+        Cursor fila = baseDeDatos.rawQuery("select nombre, apellido from persona", null);
+
+        if(fila.moveToFirst()){
             lista.add(fila.getString(1));
             baseDeDatos.close();
         }else{
             baseDeDatos.close();
         }
-
     }
 
 
